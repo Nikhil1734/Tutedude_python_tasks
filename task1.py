72e5cb4c20c9d0aea5e3b34b17dd4b160bb5f55e
Nikhil1734 <nikhilchopra700@gmail.com>
2025-11-15 15:11:51 +0530

    first_commit

---
+++ task1.py
@@ -0,0 +1,10 @@
+x=float(input("Enter first number: "))
+y=float(input("Enter second number: "))
+add=x+y
+sub=x-y
+mul=x*y
+div=x/y
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication;: ",mul)
+print("Division: ",div)
---
+++ task2.py
@@ -0,0 +1,5 @@
+first_name=input("Enter first Name: ")
+last_name=input("Enter last Name: ")
+full_name=first_name + " " + last_name
+#print(full_name)
+print("Hello, "+ full_name +"!"+ " Welcome to Python program.")
