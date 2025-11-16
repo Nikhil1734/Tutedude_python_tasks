cea80a3efe2070fa874bf1c6850a2adbbeb61eb7
Nikhil1734 <nikhilchopra700@gmail.com>
2025-11-17 00:17:21 +0530

    second assignment

--- task2.py
+++ task2.py
@@ -2,4 +2,6 @@ first_name=input("Enter first Name: ")
 last_name=input("Enter last Name: ")
 full_name=first_name + " " + last_name
 #print(full_name)
-print("Hello, "+ full_name +"!"+ " Welcome to Python program.")
+#print("Hello, "+ full_name +"!"+ " Welcome to Python program.")
+#print(f"Hello {full_name}! Welcome to python program.")
+print("Hello {}! Welcome to pythom program.".format(full_name))
---
+++ task3.py
@@ -0,0 +1,2 @@
+num=int(input("Enter any integer: "))
+print(num, "is an even number") if num % 2 == 0 else print(num, "is an odd number")
